29436e01717cb92db032f32864a975226cf08776
dashashurinova <dasha.shurinova@yandex.ru>
2023-07-26 12:31:53 +0700

    heart

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
+// pop up window
 let editButton = document.querySelector('.body__section1_edit');
 let saveButton = document.querySelector('.popUpWindow__container_button');
 let closeButton = document.querySelector('.popUpWindow__container_close-button');
 let editBlock = document.querySelector('.popUpWindow');
-
-
 
 editButton.addEventListener('click',function(){
     editBlock.classList.toggle('popUpWindow_visibility');
@@ -14,3 +13,16 @@ closeButton.addEventListener('click',function(){
 saveButton.addEventListener('click',function(){
     editBlock.classList.toggle('popUpWindow_visibility');
 })
+
+// heart button
+
+let heartButton = document.querySelectorAll('.main__card_heart');
+for (let i=0; i<heartButton.length; i++){
+    heartButton[i].addEventListener('click',function(){
+    heartButton[i].classList.toggle('blackHeart');
+})}
+
+console.log(heartButton[0].classList.contains('blackHeart'));
+console.log(heartButton[1].classList.contains('main__card_heart'));
+console.log(heartButton[2].classList.contains('blackHeart'));
+console.log(heartButton[3].classList.contains('blackHeart'));
